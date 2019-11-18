d5b0e18dbdffe255c650a44e3239feb9041053f9
Mike Hake <mike.hake@billgo.com>
2019-11-18 16:55:22 -0700

    Add aws amplify

---
+++ src/AuthenticatedApp.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Auth} from "aws-amplify";
+import Amplify from "aws-amplify";
+import { ConfirmSignIn, ForgotPassword, RequireNewPassword, SignIn, VerifyContact, withAuthenticator } from 'aws-amplify-react';
+import App from './App';
+
+const GRAPHQL_URL = '/graphql';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: process.env.REACT_APP_AWS_REGION,
+    userPoolId: process.env.REACT_APP_AWS_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_AWS_USER_POOL_WEB_CLIENT_ID,
+  },
+  API: {
+    // Note the API configuration adds `graphql_endpoint_iam_region`. This is to
+    // sign requests that are passing through API gateway as documented here:
+    // https://aws-amplify.github.io/docs/js/api#set-custom-request-headers-for-graphql
+    // It appears that we can either do this, or set the Authorization header explicitly
+    // via the `graphql_headers` property. Dont know which is preferred.
+    graphql_headers: async () => {
+      return Auth.currentSession()
+        .then(session => {
+          return { 'Authorization': session.idToken.jwtToken}
+        }).catch(err => console.log(err));
+    },
+    graphql_endpoint: GRAPHQL_URL,
+  }
+});
+
+export default withAuthenticator(App, {
+  includeGreetings: false,
+  authenticatorComponents: [
+    <SignIn/>,
+    <ConfirmSignIn/>,
+    <VerifyContact/>,
+    <ForgotPassword/>,
+    <RequireNewPassword />
+  ]
+});
