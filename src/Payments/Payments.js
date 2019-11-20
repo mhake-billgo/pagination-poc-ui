8515ac959d7ccbd859e7fba6508e3bc83353d739
Mike Hake <mike.hake@billgo.com>
2019-11-20 10:39:56 -0700

    first step to pagination. next page

--- src/Payments/Payments.js
+++ src/Payments/Payments.js
@@ -5,10 +5,10 @@ import './payments.css';
 
 
 const PAYMENTS = gql`  
-        query supplier($id: String!){
+        query supplier($id: String! $pageSize: Int! $after: String){
             supplier(id: $id){
                 legalName
-                transactionsConnection (first: 10000) {
+                transactionsConnection (first: $pageSize after: $after) {
                     totalCount
                     pageInfo {
                         startCursor
@@ -99,36 +99,42 @@ const PAYMENTS = gql`
         }
 `;
 
+const getDateStr = (timestamp) => {
+    const date = new Date(parseInt(timestamp));
+    return `${date.getFullYear()}-${(date.getMonth()+1)}-${(date.getDate()+1)}`;
+};
+
 const renderTableData = (edges) => {
     return edges.map((edge, index) => {
-        const { accountNumber, payFrom, grossAmount } = edge.node;
+        const { accountNumber, payFrom, grossAmount, depositTimestamp } = edge.node;
         return (
           <tr key={index}>
               <td>{payFrom.name}</td>
               <td>{grossAmount}</td>
               <td>{accountNumber}</td>
-
+              <td>{getDateStr(depositTimestamp)}</td>
           </tr>
-        )
+        );
     })
 };
 
 export default function Payments(props) {
   const {supplierId} = props;
-  const { loading, error, data } = useQuery(PAYMENTS,{variables: { id: supplierId }});
+  const { loading, error, data, refetch } = useQuery(PAYMENTS,{variables: {id: supplierId, pageSize: 20}});
 
   const edges = data ? data.supplier.transactionsConnection.edges : [];
   const pageInfo = data ? data.supplier.transactionsConnection.pageInfo : undefined;
   const totalCount = data ? data.supplier.transactionsConnection.totalCount : undefined;
 
+  const nextPage = () => {
+      console.log('next');
+      refetch({id: supplierId, pageSize: 20, after:pageInfo.endCursor});
+  };
+
   console.log('Payments loading, data, error, pageInfo:', loading, data, error, pageInfo);
   return (
     <div className='payments'>
         <h2>PAYMENTS</h2>
-
-        { pageInfo != null &&
-            <span>Displaying {pageInfo.startCursor} to {pageInfo.endCursor} of: {totalCount}</span>
-        }
 
         <table>
             <tbody>
@@ -136,10 +142,19 @@ export default function Payments(props) {
                     <th>From</th>
                     <th>Amount</th>
                     <th>Account</th>
+                    <th>Date</th>
                 </tr>
                 {renderTableData(edges)}
             </tbody>
         </table>
+
+        { pageInfo != null &&
+        <React.Fragment>
+            <span>Displaying {pageInfo.startCursor} to {pageInfo.endCursor} of: {totalCount}</span>
+            <button>Previous</button>
+            <button onClick={nextPage}>Next</button>
+        </React.Fragment>
+        }
     </div>
 
   );
