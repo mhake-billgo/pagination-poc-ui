5eab353ad21c555e9c7d75b92bec4745786eb079
Mike Hake <mike.hake@billgo.com>
2019-11-19 20:11:05 -0700

    header styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,10 +4,9 @@ export default function Header(props) {
   const {supplierName, userEmail, onLogout} = props;
 
   return (
-    <header>
-      <span>{supplierName}</span>
-      <span>{userEmail}</span>
-      <button className='btn btn-full' onClick={onLogout}>logout</button>
+    <header className='toolbar'>
+      <div className='toolbar__user'><span>{supplierName}</span> <span>{userEmail}</span> </div>
+      <button className='btn btn-full toolbar__logout' onClick={onLogout}>logout</button>
     </header>
   );
 }
